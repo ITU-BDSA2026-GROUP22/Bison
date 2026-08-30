ea45dd1baf39d4f2d5b07443c5f0a4e6b3b3f5a0
Sebliend <seblienddev@gmail.com>
2026-08-30 16:56:31 +0200

    Completed the week 1 task of adding the 'dotnet run -- read' and 'dotnet run -- observe'. To get some experience, each individual group member completed the task on their own PC while talking and helping each other out. Afterwards, I pushed my version of the completed task to git.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+string fileName = "bison_observe_cli_db.csv";
+
+
+if (args.Length == 0) {
+    Console.WriteLine("Invalid command");
+    return;
+}
+
+string command = args[0];
+if (command == "read") {
+    read_observations(); 
+} else if (command == "observe") {
+    add_observation();
+} else {
+    Console.WriteLine("Invalid command");
+}
+
+void read_observations() {
+    Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+    using (StreamReader reader = new StreamReader(fileName)) {
+        string? line;
+
+        reader.ReadLine();
+
+        while ((line = reader.ReadLine()) != null) {
+            string[] X = CSVParser.Split(line);
+
+            string author = X[0];
+            string observation = X[1].Trim('"');
+
+            long timestamp = long.Parse(X[2]);
+
+            DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime();
+
+            Console.WriteLine($"{author} @ {date:MM/dd/yy HH:mm:ss}: {observation}");
+        }
+    }
+}
+
+void add_observation() {
+    string observation = args[1];
+    string author = Environment.UserName;
+    long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        observation = observation.Replace("\"", "\"\"");
+
+        using (StreamWriter writer = File.AppendText(fileName)) {
+            writer.WriteLine($"{author},\"{observation}\",{timestamp}");
+        }
+
+        Console.WriteLine("Observation added.");
+}
