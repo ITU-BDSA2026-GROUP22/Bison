8f2cc2079db5639d77c120f56402977c8a6b13d9
AnimeNerd445 <hailongtony@gmail.com>
2026-09-16 15:32:15 +0200

    Add CSV database web service endpoints to our project
    Co-authored-by: Larzzer <Lars@jantzen.dk>
    Co-authored-by: DerpyUwU <alexanderkledal@gmail.com>
    Co-authored-by: AnimeNerd445 <hailongtony@gmail.com>
    Co-authored-by: Sebliend <SebliendDev@gmail.com>

--- src/Bison.CSVDBService/Program.cs
+++ src/Bison.CSVDBService/Program.cs
@@ -1,6 +1,22 @@
+using SimpleDB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IDatabaseRepository<Observation>>(new CSVDatabase<Observation>("observations.csv"));
+builder.Services.AddSingleton<IDatabaseRepository<Comment>>(new CSVDatabase<Comment>("comments.csv"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// observations
+app.MapGet("/observations", (IDatabaseRepository<Observation> db) => db.Read());
+app.MapPost("/observation", (IDatabaseRepository<Observation> db, Observation observation) => db.Store(observation));
+
+// comments
+app.MapGet("/comments", (IDatabaseRepository<Comment> db) => db.Read());
+app.MapPost("/comment", (IDatabaseRepository<Comment> db, Comment comment) => db.Store(comment));
+
 
 app.Run();
+
+public record Observation(string Author, string Message, long Timestamp);
+public record Comment(int ObservationId, string Author, string Message, long Timestamp);
